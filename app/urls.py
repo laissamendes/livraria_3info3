da98c07f91d0b9c08d8547c1e9512c0434c1f721
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-03-18 14:45:21 -0300

    Criação da API categoria e Criação da API editora

--- app/urls.py
+++ app/urls.py
@@ -10,10 +10,15 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import UserViewSet
+from core.views import CategoriaViewSet # nova linha
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet
 
 router = DefaultRouter()
 
+router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
